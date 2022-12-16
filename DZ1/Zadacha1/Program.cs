e34bc5e01ead0d723c9d9db363773a16575c61ca
VladimirBoychenko <boichenkova6644@icloud.com>
2022-12-16 06:22:29 +0300

    Sdelali zadacha 1

---
+++ DZ1/Zadacha1/Program.cs
@@ -0,0 +1,18 @@
+﻿int a1 = 5;
+int b1 = 7;
+int a2 = 44;
+int b2 = 15;
+double a3 = -9;
+double b3 = -3;
+if(a1 > b1)
+    Console.WriteLine("a1 bolshe b1");
+else
+    Console.WriteLine("b1 bolshe a1");
+if(a2 > b2)
+    Console.WriteLine("a2 bolshe b2");
+else
+    Console.WriteLine("b2 bolshe a2");
+if(a3 > b3)
+    Console.WriteLine("a3 bolshe b3");
+else
+    Console.WriteLine("b3 bolshe a3");
