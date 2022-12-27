5bda8febc425fc05cef81edd9b00242cb31bac23
VladimirBoychenko <boichenkova6644@icloud.com>
2022-12-27 15:04:03 +0300

    Сделали дз 3

---
+++ DZ3/Zadacha19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.Clear();
+System.Console.Write("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while ( a < 9999 || a > 100000){
+    System.Console.WriteLine("Пиши правильное число от 10000 до 99999");
+    a = Convert.ToInt32(Console.ReadLine()); 
+}
+int num1 = a % 10; 
+int num2 = a / 10 % 10;
+int num3 = a / 100 % 10; 
+int num4 = a / 1000 % 10; 
+int num5 = a / 10000 % 10; 
+int result = ((((num1*10+num2)*10+num3)*10+num4)*10+num5);
+if (a == result){
+    System.Console.WriteLine($"Число {a} палиндром, результат {result}");
+}
+else{
+    System.Console.WriteLine($"Число {a} нифига не палиндром");
+}
---
+++ DZ3/Zadacha21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+System.Console.Write("Ведите число Х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ведите число y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ведите число z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ведите число Х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ведите число y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ведите число z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Round(Math.Sqrt(Math.Pow((x1 - x2),2)+Math.Pow((y1 - y2),2)+Math.Pow((z1 - z2),2)),2);
+
+
+
+System.Console.WriteLine($"Растояние между тремя точками: {result}");
